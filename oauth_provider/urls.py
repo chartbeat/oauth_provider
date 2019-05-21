1828fbb2b6faa624d2dde7fb43080ca45a89ea30
ronnie <ron@chart.bt>
2019-05-21 18:19:40 -0400

    fix pattern import

--- oauth_provider/urls.py
+++ oauth_provider/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import *
 
 from views import request_token, user_authorization, access_token
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^request_token/$',    request_token,      name='oauth_request_token'),
     url(r'^authorize/$',        user_authorization, name='oauth_user_authorization'),
     url(r'^access_token/$',     access_token,       name='oauth_access_token'),
-)
+]
